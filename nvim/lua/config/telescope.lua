41b629f8ca87497dc700428c52aadccf86636314
songji.zeng <lekyzsj@gmail.com>
2022-05-19 22:37:23 +0800

    telescope mappings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -4,6 +4,12 @@ local actions   = require('telescope.actions')
 telescope.setup {
   defaults = {
     file_ignore_patterns = { 'node_modules', '.git', '.idea', '.vscode' },
+    mappings = {
+      i = {
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+      }
+    }
   },
   pickers = {
     find_files = { hidden = true },
